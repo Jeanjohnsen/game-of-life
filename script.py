57d4cdb5aad494938c7baf52405d671d901d5a6e
Jean Johnsen <Geniusnh4@gmail.com>
2020-05-26 11:22:10 +0200

    First edit

--- script.py
+++ script.py
@@ -9,6 +9,13 @@
 # The rest of the comments will be in danish for educational purposes for danish students
 
 
+# Code survival guide:
+# * infront of a variable collects all the positional arguments in a tuple.
+# ** infront of a variable collects all the keyword arguments in a dictionary.
+#
+
+
+
 import math
 
 import numpy as np
@@ -62,6 +69,41 @@ class Rules:
     def state(self, n, m):
         return self.sigma_two(n, self.lerp(self.B1, self.R1, alive), self.lerp(self.B2, self.R2, alive))
 
+
+"""
+Laver en cirkel med fadede kanter.
+
+Hvis du vil have cirklen centreret i midten
+af matricen så sæt roll = False.
+Normalt centreres det ved funktionens ekstremum
+
+overgangen af faden skalerer med strørrelsen af gridden.
+Jeg er ikke sikker på matematikken bagved men det var hvad der stod her:
+    https://0fps.net/2012/11/19/conways-game-of-life-for-curved-surfaces-part-1/
+"""
+def log_blur(size, radius, roll=True, log_r=None):
+
+    x, y = size
+
+    # Få koordinaterne fra hvert punkt
+    xx, yy = np.mgrid[:x, :y]
+
+    # Distancen mellem hvert punkt og midten
+    _radius_ = np.sqrt((xx - x/2)**2 + (yy - y/2)**2)
+
+    # Skaler udfra overgangs vidden
+    if log_r is None:
+        log_r = math.log.(min(*size), 2)
+
+    # Med store radiuser er der chance for exponent overflow
+    # men hvis 1/(1+inf) så er det fint
+    with np.errstate(over='ignore'):
+        log_norm = 1 / (1 + np.exp(log_r * (_radius_ - radius)))
+
+    if roll:
+        log_norm = np.roll(log_norm, y//2, axis=0)
+        log_norm = np.roll(log_norm, x//2, axis=1)
+    return log_norm
 
 # Det samme som run
 if __name__ == '__main__':
